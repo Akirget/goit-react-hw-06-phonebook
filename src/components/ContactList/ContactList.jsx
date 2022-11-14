b8eedd58af862d18ff2521367fe0bc7a608f200f
Kirhet Oleksandr <57485036+Akirget@users.noreply.github.com>
2022-11-14 09:15:14 +0100

    Initial commit

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,34 @@
+import style from './ContactList.module.css';
+import PropTypes from 'prop-types';
+
+const ContactList = ({ contacts, onDeleteContact }) => (
+  <ul className={style.list}>
+    {contacts.map(({ id, name, number }) => (
+      <li key={id} className={style.item}>
+        <p className={style.contact}>
+          {name} - {number}
+        </p>
+        <button
+          className={style.btn}
+          type="submit"
+          onClick={() => onDeleteContact(id)}
+        >
+          Delete
+        </button>
+      </li>
+    ))}
+  </ul>
+);
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  onDeleteContact: PropTypes.func.isRequired,
+};
+
+export default ContactList;
